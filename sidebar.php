8f9ac7efaee890c847743606f96ded98aeafecf2
lkm543 <lkm543@gmail.com>
2020-11-24 05:53:40 +0800

    Update

--- sidebar.php
+++ sidebar.php
@@ -9,9 +9,9 @@
     <li class='tab' data-tabid="Chapter00"><a href="#Chapter00">Ch 00 C/C++快速複習</a></li>
     <li class='tab' data-tabid="Chapter01"><a href="#Chapter01">Ch 01 資結演算法入門</a></li>
     <li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Ch 02 複雜度估算(Complexity)</a></li>
-    <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 (資結)Linked List</a></li>
+    <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 (資結)鏈結串列(Linked List)</a></li>
     <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
-    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 (資結)Hash table</a></li>
+    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 (資結)雜湊表(Hash table)</a></li>
     <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Ch 05 (資結)堆疊(stacks)與佇列(queues)</a></li>
     <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 (資結)二元樹</a></li>
     <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
