46af3e152d258a20a567c0ffbed63d788bc2c623
lkm543 <lkm543@gmail.com>
2020-12-02 12:23:33 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -11,17 +11,20 @@
     <li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Ch 02 複雜度估算(Complexity)</a></li>
     <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 (資結)鏈結串列(Linked List)</a></li>
     <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
-    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 (資結)堆疊(stack)與佇列(queue)</a></li>
+    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 (資結)堆疊(Stack)與佇列(Queue)</a></li>
     <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Ch 05 (資結)雜湊表(Hash table)</a></li>
     <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
-    <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 (資結)二元樹</a></li>
-    <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Ch 07 (資結)圖論(graph)─BFS與DFS</a></li>
-    <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Ch 08 (資結)圖論(graph)─Shortest Path</a></li>
+    <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 (資結)二元樹(Binary Tree)</a></li>
+    <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Ch 07 (資結)二元樹(Binary Tree)補充</a></li>
+    <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Ch 08 (資結)紅黑樹(Red Black Tree)</a></li>
+    <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Ch 09 (資結)圖論(Graph)─BFS與DFS</a></li>
     <li class='tab' data-tabid="HW8"><a href="#HW3">Homework 3</a></li>
-    <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Ch 09 (演算)排序(sorting)</a></li>
-    <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Ch 10 (演算)搜尋(searching)</a></li>
+    <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Ch 10 (資結)圖論(Graph)─Minimum Spanning Tree</a></li>
+    <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 (資結)圖論(Graph)─Shortest Path</a></li>
     <li class='tab' data-tabid="HW4"><a href="#HW4">Homework 4</a></li>
-    <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 (演算)貪心法則(greedy method)</a></li>
-    <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 (演算)動態規劃(dynamic programming)</a></li>
+    <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 (演算)搜尋(Search)</a></li>
+    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 (演算)排序(Sort)</a></li>
+    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 (演算)貪婪演算法(Greedy Algorithm)</a></li>
+    <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Ch 15 (演算)動態規劃(Dynamic Programming)</a></li>
     <li class='tab' data-tabid="HW5"><a href="#HW5">Homework 5</a></li>
 </ul>
