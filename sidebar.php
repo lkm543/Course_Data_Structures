cb389c3fd2861662227f014a1afec0317eef137a
lkm543 <lkm543@gmail.com>
2020-12-15 23:44:33 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -23,14 +23,13 @@
     <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 深度優先搜尋(DFS)</a></li>
     <li class='tab' data-tabid="HW4"><a href="#HW4">Homework 4</a></li>
     <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 最短路徑(Shortest Path)</a></li>
-    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 最短路徑補充(Shortest Path)</a></li>
-    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 貪婪演算法(Greedy Algorithm)</a></li>
-    <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Ch 15 分治法(Divide and Conquer)</a></li>
-    <li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Ch 16 動態規劃(Dynamic Programming)</a></li>
+    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 貪婪演算法(Greedy Algorithm)</a></li>
+    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 分治法(Divide and Conquer)</a></li>
+    <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Ch 15 動態規劃(Dynamic Programming)</a></li>
     <li class='tab' data-tabid="HW5"><a href="#HW5">Homework 5</a></li>
-    <li class='tab' data-tabid="Chapter17"><a href="#Chapter17">Ch 17 搜尋(Search)</a></li>
-    <li class='tab' data-tabid="Chapter18"><a href="#Chapter18">Ch 18 排序(Sort)</a></li>
-    <li class='tab' data-tabid="Chapter19"><a href="#Chapter19">Ch 19 最小生成樹(Minimum Spanning Tree)</a></li>
-    <li class='tab' data-tabid="Chapter20"><a href="#Chapter20">Ch 20 AVL樹</a></li>
-    <li class='tab' data-tabid="Chapter21"><a href="#Chapter21">Ch 21 平攤分析(Amortized Analysis)</a></li>
+    <li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Ch 16 搜尋(Search)</a></li>
+    <li class='tab' data-tabid="Chapter17"><a href="#Chapter17">Ch 17 排序(Sort)</a></li>
+    <li class='tab' data-tabid="Chapter18"><a href="#Chapter18">Ch 18 最小生成樹(Minimum Spanning Tree)</a></li>
+    <li class='tab' data-tabid="Chapter19"><a href="#Chapter19">Ch 19 AVL樹</a></li>
+    <li class='tab' data-tabid="Chapter20"><a href="#Chapter20">Ch 20 平攤分析(Amortized Analysis)</a></li>
 </ul>
