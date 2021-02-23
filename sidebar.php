087d5dd9691f4eeeb50d95d9502567bc53449bde
lkm543 <lkm543@gmail.com>
2021-02-23 17:25:16 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,35 +5,25 @@
 
 <h2 class="star">課程目錄</h2>
 <ul class="sb_menu etabs">
-    <li class='tab' data-tabid="Introduction"><a href="#Introduction">Introduction 課程簡介</a></li>
     <li class='tab' data-tabid="Chapter00"><a href="#Chapter00">Ch 00 C/C++快速複習</a></li>
+    <li class='tab' data-tabid="Introduction"><a href="#Introduction">Introduction 課程簡介</a></li>
     <li class='tab' data-tabid="Chapter01"><a href="#Chapter01">Ch 01 資結演算法入門</a></li>
     <li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Ch 02 複雜度估算(Complexity)</a></li>
-    <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 向量(Vector)_TBA</a></li>
-    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 鏈結串列(Linked List)</a></li>
+    <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 抽象資料型別(Abstract Data Type)</a></li>
     <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
-    <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Ch 05 堆疊(Stack)與佇列(Queue)</a></li>
-    <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 雜湊表(Hash table)</a></li>
+    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 陣列(Array)</a></li>
+    <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Ch 05 C++ STL簡介</a></li>
+    <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 向量(Vector)</a></li>
+    <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Ch 07 字串(String)</a></li>
+    <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Ch 08 鏈結串列(Linked List)</a></li>
     <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
-    <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Ch 07 二元樹(Binary Tree)</a></li>
-    <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Ch 08 二元樹(Binary Tree)補充</a></li>
-    <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Ch 09 AVL樹(Adelson-Velsky and Landis Tree)</a></li>
-    <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Ch 10 2-3樹與紅黑樹(Red Black Tree)</a></li>
+    <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Ch 09 堆疊(Stack)</a></li>
+    <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Ch 10 佇列(Queue)</a></li>
     <li class='tab' data-tabid="HW3"><a href="#HW3">Homework 3</a></li>
-    <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 圖論(Graph)</a></li>
-    <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 廣度優先搜尋(BFS)</a></li>
-    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 深度優先搜尋(DFS)</a></li>
+    <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 雜湊表(Hash table)</a></li>ㄤ
+    <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 二元樹(Binary Tree)基礎</a></li>
     <li class='tab' data-tabid="HW4"><a href="#HW4">Homework 4</a></li>
-    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 最小生成樹(Minimal Spanning Tree)</a></li>
-    <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Ch 15 網路流(Flow Network)_TBA</a></li>
-    <li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Ch 16 最短路徑(Shortest Path)</a></li>
+    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 二元樹相關應用</a></li>
+    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 二元平衡樹</a></li>
     <li class='tab' data-tabid="HW5"><a href="#HW5">Homework 5</a></li>
-    <!--
-    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 分治法(Divide and Conquer)</a></li>
-    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 動態規劃(Dynamic Programming)</a></li>
-    <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Ch 15 貪婪演算法(Greedy Algorithm)</a></li>
-    <li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Ch 16 搜尋(Search)</a></li>
-    <li class='tab' data-tabid="Chapter17"><a href="#Chapter17">Ch 17 排序(Sort)</a></li>
-    <li class='tab' data-tabid="Chapter20"><a href="#Chapter20">Ch 20 平攤分析(Amortized Analysis)</a></li>
-    -->
 </ul>
