95c11151db13df27e191c03c3b291ba371e66627
lkm543 <lkm543@gmail.com>
2021-03-24 11:36:08 +0800

    modify link

--- News.php
+++ News.php
@@ -3,6 +3,7 @@
 <h2>重要公告</h2>
 <ul>
 	<li>基礎班的<a href="https://www.lkm543.site/Course/Cpp/">連結</a>，帳號CPP、密碼ILOVECPP</li>
+	<li>演算法班的<a href="https://www.lkm543.site/Algorithm">連結</a>，帳號AL、密碼ILOVEAL</li>
 	<li>本門課的<a href="https://discord.gg/vY9NU53Mzn">Discord連結</a></li>
 	<li>不是本門課但可以參考的<a href="https://www.facebook.com/%E7%94%9F%E6%B4%BB%E4%B8%AD%E7%9A%84%E7%A8%8B%E5%BC%8F-108418124301138">粉絲專頁</a></li>
 	<li>無線網路Wifi帳號密碼(2021上半年)</li>
